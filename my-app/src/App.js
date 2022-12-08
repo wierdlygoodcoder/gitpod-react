f25e7eadd0fe30f25925cbe05e31867d2287e357
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-12-08 23:27:02 +0000

    finished props video

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps />
+      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
     </div>
   );
 }
--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const FunctionalGreetingWithProps = () => <h1>Hello</h1>
+const FunctionalGreetingWithProps = (props) => {
+console.log(props);
+return <h1>Hello, {props.name} I see your {props.age} years old; {props.greeting}</h1>;
+}
 
 export default FunctionalGreetingWithProps;
