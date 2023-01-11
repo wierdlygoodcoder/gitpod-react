82fc2662ab6248345d8543da5481c637c685d101
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-01-11 23:26:15 +0000

    finished installing es7 snippets

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
+      <StatefulGreetingWithPrevState/>
     </div>
   );
 }
---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,53 @@
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+//rfce tab creates react functional component
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+//rafce arrow function
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+//rce react class
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+//rconst adds a defult constructor for a class that contains props as arguments
+
+// console.log(first)
+//clg
+
+// console.log('first', first)
+// clo object loging 
